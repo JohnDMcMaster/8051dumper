88f0934c2de7a4377d140610df15c44a8c6e4679
Mark J. Blair <nf6x@nf6x.net>
2020-05-09 22:43:25 -0700

    First dumper code that seems to work with 8031 testing.

--- firmware/8051dumper.asm
+++ firmware/8051dumper.asm
@@ -8,6 +8,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GPIOs
+;;
 OUT_EA          ALIAS   "P1.0"      ; Inverted and connected to EAn pin
 OUT_REDLEDn     ALIAS   "P1.1"      ; Red LED in RESET button
 OUT_GRNLEDn     ALIAS   "P1.2"      ; Green LED in START button
@@ -19,13 +20,29 @@ IN_R4Kn         ALIAS   "P1.7"      ; Dump size knob: 8051 (4k)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Internal RAM (uninitialized data segment)
+;;
                 SEGU    "IRAM"
 
 ; 0x00..0x1F contains the four register banks.
 ; 0x20..0x2F is the bit addressable area.
 ; 0x30..0x7F is the scratch pad area.
 
-                ORG     0x0008
+                ORG     0x0030
+IROMSIZE:       DS      1           ; High byte of internal ROM size
+CKSUM:          DS      1           ; Checksum accumulator for line
+HINYBBLE:       DS      1           ; Temp buffer for HEXBYTE
+LONYBBLE:       DS      1           ; Temp buffer for HEXBYTE
+
+; Intel Hex line buffer for 16 byte record
+BUF_START:      DS      1           ; Start code ":"
+BUF_BYTECOUNT:  DS      2           ; Byte count "10"
+BUF_ADDR:       DS      4           ; Start address of record
+BUF_RECTYPE:    DS      1           ; Record type
+BUF_DATA:       DS      32          ; Record data
+BUF_CKSUM:      DS      2           ; Checksum
+BUF_EOL:        DS      3           ; CRLF and terminating NUL
+
+
 
 ; Rest of RAM is stack. Initial stack pointer should be 1 byte before
 ; beginning of stack because PUSH operations pre-increment the stack pointer.
@@ -34,35 +51,36 @@ STACK:          DS      0x0080-$
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Code segment starting at base of external program memory EEPROM
+;;
                 SEG     "code"
 
                 ; Reset vector (3 bytes)
                 ORG     0x0000
-RESET:          LJMP    INIT
+RESET:          LJMP    MAIN
 
                 ; External Request 0 Interrupt Service Routine (8 bytes)
                 ORG     0x0003
-ER0_ISR:        SJMP    RESET
+ER0_ISR:        LJMP    ERROR
 
                 ; Internal Timer/Counter 0 Interrupt Service Routine (8 bytes)
                 ORG     0x000B
-ITC0_ISR:       SJMP    RESET
+ITC0_ISR:       LJMP    ERROR
 
                 ; External Request 1 Interrupt Service Routine (8 bytes)
                 ORG     0x0013
-ER1_ISR:        SJMP    RESET
+ER1_ISR:        LJMP    ERROR
 
                 ; Internal Timer/Counter 1 Interrupt Service Routine (8 bytes)
                 ORG     0x001B
-ITC1_ISR:       SJMP    RESET
+ITC1_ISR:       LJMP    ERROR
 
                 ; Internal Serial Port Interrupt Service Routine (8 bytes)
                 ORG     0x0023
-ISP_ISR:        SJMP    RESET
+ISP_ISR:        LJMP    ERROR
 
                 ; Some other damn interrupt
                 ORG     0x002B
-FOO_ISR:        SJMP    RESET
+FOO_ISR:        LJMP    ERROR
 
 
 
@@ -71,17 +89,22 @@ FOO_ISR:        SJMP    RESET
 ;; at 0x8000. We will raise the EAn pin with a GPIO in order to overlay
 ;; internal ROM at address 0x0000. We will not use any interrupts because
 ;; interrupt vectors will contain unpredictable code from internal ROM.
-
+;;
                 ORG     0x0032
                 RORG    0x8032
 
 IDSTR:          DB      "8051dumper v1.0 by NF6X", 0x0D, 0x0A, 0x00
+KNOBSTR:        DB      "ERROR: Could not read ROM size knob!", 0x0D, 0x0A, 0x00
+ENDSTR:         DB      ":00000001FF", 0x0D, 0x0A, 0x00
 
-;; 
-INIT:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Main entry point
+;;
+MAIN:
                 MOV     IE, #0x00               ; Disable all interrupts
                 MOV     P1, #0xFF               ; GPIOs all input/high
                 MOV     SP, #STACK-1            ; Initialize stack pointer
+                CLR     OUT_EA                  ; Select INTERNAL ROM at 0x0000
 
 
 ; Configure serial port for Mode 1, 9600 8n1
@@ -99,7 +122,7 @@ INIT:
 
 ; Send ID string 
                 MOV     DPTR, #IDSTR
-                ACALL   SENDSTR
+                ACALL   SENDROM
 
 
 ; Blink green LED until green START button pressed.
@@ -107,26 +130,164 @@ INIT:
 ; button every 100ms.
 WAITSTART:      CLR     OUT_GRNLEDn             ; On for 100ms
                 ACALL   DELAY100ms
-                JNB     IN_GRNBTNn, DUMP        ; Button pressed?
+                JNB     IN_GRNBTNn, READKNOB    ; Button pressed?
                 SETB    OUT_GRNLEDn             ; Off for 400ms
                 ACALL   DELAY100ms
-                JNB     IN_GRNBTNn, DUMP        ; Button pressed?
+                JNB     IN_GRNBTNn, READKNOB    ; Button pressed?
                 ACALL   DELAY100ms
-                JNB     IN_GRNBTNn, DUMP        ; Button pressed?
+                JNB     IN_GRNBTNn, READKNOB    ; Button pressed?
                 ACALL   DELAY100ms
-                JNB     IN_GRNBTNn, DUMP        ; Button pressed?
+                JNB     IN_GRNBTNn, READKNOB    ; Button pressed?
                 ACALL   DELAY100ms
-                JNB     IN_GRNBTNn, DUMP        ; Button pressed?
+                JNB     IN_GRNBTNn, READKNOB    ; Button pressed?
                 SJMP    WAITSTART
 
 ; Read dump size knob and begin dumping code.
-DUMP:           SETB    OUT_GRNLEDn             ; Green LED off
-                CLR     OUT_REDLEDn             ; Red LED on
-                CLR     OUT_EA                  ; Select INTERNAL ROM at 0x0000
+READKNOB:       SETB    OUT_GRNLEDn             ; Green LED off
+ 
+.TST32K:        JB      IN_R32Kn, .TST16K       ; Knob set to 32k?
+                MOV     IROMSIZE, #0x80         ; Yes, 32k
+                SJMP    INITBUF
+.TST16K:        JB      IN_R16Kn, .TST8K        ; Knob set to 16k?
+                MOV     IROMSIZE, #0x40         ; Yes, 16k
+                SJMP    INITBUF
+.TST8K:         JB      IN_R8Kn, .TST4K         ; Knob set to 8k?
+                MOV     IROMSIZE, #0x20         ; Yes, 8k
+                SJMP    INITBUF
+.TST4K:         JB      IN_R4Kn, .TSTFAIL       ; Knob set to 4k?
+                MOV     IROMSIZE, #0x10         ; Yes, 4k
+                SJMP    INITBUF
+.TSTFAIL:       MOV     DPTR, #KNOBSTR          ; Cannot read knob!
+                ACALL   SENDROM
+                LJMP    ERROR
 
-                
 
+; Initialize the line buffer and ROM data pointer
+INITBUF:        MOV     BUF_START, #':'
+                MOV     BUF_BYTECOUNT, #'1'
+                MOV     BUF_BYTECOUNT+1, #'0'
+                MOV     BUF_EOL, #0x0D
+                MOV     BUF_EOL+1, #0x0A
+                MOV     BUF_EOL+2, #0x00
+                MOV     DPTR, #0000 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Dump one Intel Hex record of 16 bytes at a time until done.
+;; Toggle red LED for each line to show activity.
+;;
+DUMPLOOP:       MOV     A, DPH                  ; Has DPTR hit end of IROM?
+                CJNE    A, IROMSIZE, DUMPRECORD
+                SJMP    EOF                     ; Finished with IROM
+
+; Dump one 16-byte record
+DUMPRECORD:
+                CPL     OUT_REDLEDn
+                MOV     CKSUM, #0x10            ; Init checksum w/ record length
+                MOV     R0, #BUF_ADDR           ; Point to address field
+                MOV     A, DPH                  ; High address byte
+                ACALL   HEXBYTE
+                MOV     A, DPL                  ; Low address byte
+                ACALL   HEXBYTE
+                CLR     A                       ; Record type
+                ACALL   HEXBYTE
+
+DUMPBYTE:       CLR     A                       ; Read byte from IROM
+                MOVC    A, @A+DPTR
+                ACALL   HEXBYTE                 ; Add it to line buffer
+                MOV     A, DPL                  ; End of record?
+                ANL     A, #0x0F 
+                CLR     C
+                SUBB    A, #0x0F
+                JZ      EOR                     ; Yes, at end of 16-byte record
+                INC     DPTR                    ; No, increment IROM pointer
+                SJMP    DUMPBYTE                ; Add next byte
+
+EOR:            MOV     A, CKSUM                ; Add checksum to record
+                CPL     A
+                ADD     A, #1
+                ACALL   HEXBYTE
+                ACALL   SENDRECORD              ; Send out this record
+                INC     DPTR
+
+                SJMP    DUMPLOOP
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Send out end of file record, turn green LED on and red LED off,
+;; and loop forever.
+;;
+EOF:            MOV     DPTR, #ENDSTR
+                ACALL   SENDROM
+                CLR     OUT_GRNLEDn             ; Green LED on
+                SETB    OUT_REDLEDn             ; Red LED off
                 SJMP    $
+
+               
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Add hex representation of A to buffer at R0 and accumulate checksum.
+;;
+HEXBYTE:        MOV     HINYBBLE, A             ; Add byte to checksum
+                MOV     LONYBBLE, A
+                ADD     A, CKSUM
+                MOV     CKSUM, A
+
+                MOV     A, HINYBBLE             ; Isolate high nybble
+                SWAP    A
+                ANL     A, #0x0F
+                ADD     A, #'0'
+                MOV     HINYBBLE, A
+                CLR     C
+                SUBB    A, #':'
+                JC      .LO
+                MOV     A, HINYBBLE
+                ADD     A, #7
+                MOV     HINYBBLE, A
+
+.LO:            MOV     A, LONYBBLE             ; Isolate low nybble
+                ANL     A, #0x0F
+                ADD     A, #'0'
+                MOV     LONYBBLE, A
+                CLR     C
+                SUBB    A, #':'
+                JC      .STHI
+                MOV     A, LONYBBLE
+                ADD     A, #7
+                MOV     LONYBBLE, A
+
+.STHI:          MOV     A, HINYBBLE             ; Store high nybble hex char
+                MOV     @R0, A
+                INC     R0
+                MOV     A, LONYBBLE             ; Store low nybble hex char
+                MOV     @R0, A
+                INC     R0
+
+                RET
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Send out the current record. Trashes and R0.
+;;
+SENDRECORD:     MOV     R0, #BUF_START          ; Init pointer
+.SENDCHAR:      MOV     A, @R0                  ; Get next char
+                JZ      .DONE                   ; Done if it is NUL
+                CLR     SCON_TI                 ; Send byte
+                MOV     SBUF, A
+                JNB     SCON_TI, $              ; Loop until character sent
+                INC     R0
+                SJMP    .SENDCHAR
+.DONE:          RET
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Error handler
+;; Turn on red LED and loop forever
+;;
+ERROR:          SETB    OUT_GRNLEDn             ; Green LED off
+                CLR     OUT_REDLEDn             ; Red LED on
+                SJMP    $
+
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -136,7 +297,7 @@ DUMP:           SETB    OUT_GRNLEDn             ; Green LED off
 ;;   ==> 50ms = 11059200 / (12 * 20) = 46080 = 0xB400 counts
 ;;   ==> (TH0,TL0) = 0x10000 - 0xB400 = 0x4C00
 ;; This all ignores subroutine call overhead and so forth.
-
+;;
 DELAY100ms:     CLR     TCON_TR0                ; Timer off
                 MOV     TH0, #0x4C              ; 50ms delay
                 MOV     TL0, #0x00
@@ -156,15 +317,15 @@ DELAY100ms:     CLR     TCON_TR0                ; Timer off
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Send NUL-terminated string in ROM pointed to by DPTR.
 ;; Trashes A, DPTR
-
-SENDSTR:        CLR     A                       ; Get byte from ROM
+;;
+SENDROM:        CLR     A                       ; Get byte from ROM
                 MOVC    A, @A+DPTR
                 JZ      .DONE                   ; Done if it is NUL
                 CLR     SCON_TI                 ; Send byte
                 MOV     SBUF, A
                 JNB     SCON_TI, $              ; Loop until character sent
                 INC     DPTR                    ; Send next byte
-                SJMP    SENDSTR
+                SJMP    SENDROM
 .DONE:          RET
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- firmware/fill.py
+++ firmware/fill.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 #
 
-"""Fill unused areas of 32k ROM image with a recognizable pattern."""
+"""Fill unused areas of 32k ROM image with a recognizable pattern.
+
+Each 16 byte chunk is filled with:
+  Address (2 bytes)
+  0x00
+  0x80
+  0x51
+  0x00
+  "8051dumper"
+"""
 
 import argparse
 import intelhex
@@ -20,9 +29,13 @@ if __name__ == '__main__':
 
     fw_image = intelhex.IntelHex(args.infile)
 
-    fill = bytes([0xDE, 0xAD, 0xBE, 0xEF]) * int(32768/4)
+    fill = list(b'\x00\x00\x00\x80\x51\x008051dumper' * int(32768/16))
+    for n in range(0, len(fill), 16):
+        fill[n]   = n>>8
+        fill[n+1] = n & 0xFF
+
     filled_image = intelhex.IntelHex()
-    filled_image.frombytes(fill)
+    filled_image.frombytes(bytes(fill))
     filled_image.merge(fw_image, overlap='replace')
 
     filled_image.write_hex_file(args.outfile)
